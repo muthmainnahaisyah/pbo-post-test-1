0a67e54c67ec74495fabede66ce5ee9830f9c345
muthmainnahaisyah <122006658+muthmainnahaisyah@users.noreply.github.com>
2023-09-26 15:12:18 +0800

    Musik.java

--- src/main/java/com/mycompany/posttest1/Musik.java
+++ src/main/java/com/mycompany/posttest1/Musik.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.posttest1;
 
-/**
- *
- * @author muthm
- */
 public class Musik {
     // Memberikan Property Dari Kelas Musik
     public String judul;
